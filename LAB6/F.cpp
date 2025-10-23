bfb95435e4ae370cad0dc65d5ba9b44eab2ec8c2
AmirZholdasali <zoldasaliamir@gmail.com>
2025-10-23 17:23:36 +0500

    Update F.cpp

--- LAB6/F.cpp
+++ LAB6/F.cpp
@@ -1,44 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node{
-    int data;
-    Node *right, *left;
-    Node(int x) : data(x), right(nullptr), left(nullptr){}
+struct person{
+    string name;
+    string surname;
+    double gpa;
 };
 
-
-void bt_insert(Node*& root, int val){
-    if(!root){
-        root = new Node(val);
-        return;
-    }
-    if(val < root->data) bt_insert(root->left, val);
-    else bt_insert(root->right, val);
+bool cmp (person a, person b){
+    if(a.gpa != b.gpa) return a.gpa < b.gpa;
+    if(a.surname != b.surname) return a.surname < b.surname;
+    return a.name < b.name;
 }
 
-
-int bt_triangle_count(Node* root, int &cnt){
-    if(!root) return 0;
-    if(root->right && root->left) cnt++;
-    bt_triangle_count(root->left, cnt);
-    bt_triangle_count(root->right, cnt);
-
-
-
-    return cnt;
-}
-
-
-int main(){
-    int n, x;
+int main() {
+    
+    int n, m;
+    double x;
+    string s;
+    person p[100005];
+    
     cin >> n;
-    Node* root = nullptr;
-    for(int i = 0; i<n; i++){
-        cin >> x;
-        bt_insert(root, x);
+    
+    for(int i = 1; i <= n; i++){
+        cin >> p[i].surname >> p[i].name >> m;
+        int sum_of_credits = 0; double sum_of_gpa = 0;
+        for(int j = 1; j <= m; j++){
+            cin >> s >> x;
+            if(s == "A+") sum_of_gpa += (4.0 * x);
+            if(s == "A") sum_of_gpa += (3.75 * x);
+            if(s == "B+") sum_of_gpa += (3.5 * x);
+            if(s == "B") sum_of_gpa += (3.0 * x);
+            if(s == "C+") sum_of_gpa += (2.5 * x);
+            if(s == "C") sum_of_gpa += (2.0 * x);
+            if(s == "D+") sum_of_gpa += (1.5 * x);
+            if(s == "D") sum_of_gpa += (1.0 * x);
+            if(s == "F") sum_of_gpa += (0 * x);
+            
+            sum_of_credits += x;
+        }
+        p[i].gpa = sum_of_gpa / double(sum_of_credits);
     }
-    int cnt = 0;
-    cout << bt_triangle_count(root, cnt) << endl;
+    sort(p + 1, p + 1 + n, cmp);
+    
+    for(int i = 1; i <= n; i++){
+        cout.precision(3);
+        cout << p[i].surname << ' ' << p[i].name << ' ' << fixed << p[i].gpa << '\n';
+    }
+    
     return 0;
 }
